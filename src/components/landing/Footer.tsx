b4c90c66020880f05805cb094e2c1b8a70fd1070
Renat Chernov <renat3846@gmail.com>
2023-10-12 15:02:56 +0300

    commit

---
+++ src/components/landing/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+      <div className=' text-black py-4'>
+        <div className='container mx-auto text-center'>
+          <p className='text-sm'>&copy; 2023 STRONG MIND ACADEMY. All rights reserved</p>
+        </div>
+      </div>
+    )
+  }
+  
+  export default Footer
+  
---
+++ src/redux/store.ts
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit';
+import userReducer from './features/userSlice';
+import tutorReducer from './features/tutorSlice';
+
+const store = configureStore({
+  reducer: {
+    user: userReducer,
+    tutor: tutorReducer,
+  },
+});
+
+export default store;
+
+export type RootState = ReturnType<typeof store.getState>; // Экспортируйте тип RootState
