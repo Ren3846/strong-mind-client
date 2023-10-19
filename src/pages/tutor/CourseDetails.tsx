395eee0956db34b441a2817f93728d3bc6305cc3
Renat Chernov <renat3846@gmail.com>
2023-10-19 21:52:37 +0300

    commit

--- src/pages/tutor/CourseDetails.tsx
+++ src/pages/tutor/CourseDetails.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Card, Row, Typography } from 'antd'
+import { Card, Layout, Row, Typography } from 'antd'
 import axios from 'axios'
 import { useParams } from 'react-router-dom'
 
@@ -15,27 +15,26 @@ const CourseDetails: React.FC = () => {
       .get(`/api/courses/created?id=${id}`)
       .then((response) => {
         setCourse(response.data)
+        console.log(response.data)
       })
       .catch((error) => {
         console.error(error)
       })
   }, [id])
 
-  if (!course) {
-    return <div>Loading...</div>
-  }
-
   return (
-    <Row align='middle' justify='center'>
-      <p>{id}</p>
-      <div>
-        <h1>{course.title}</h1>
-        <p>{course.about}</p>
-        <p>Category: {course.category}</p>
-        <p>Difficulty: {course.difficulty}</p>
-        <p>Price: ${course.price}</p>
-      </div>
-    </Row>
+    <Layout>
+      <Row align='middle' justify='center'>
+        <h4>{id}</h4>
+        <div>
+          <h1>{course.title}</h1>
+          <p>{course.about}</p>
+          <p>Category: {course.category}</p>
+          <p>Difficulty: {course.difficulty}</p>
+          <p>Price: ${course.price}</p>
+        </div>
+      </Row>
+    </Layout>
   )
 }
 
