1e9bf054911dcca7f1da9705ab61c255dcf7b0b3
Renat Chernov <renat3846@gmail.com>
2023-12-11 17:47:54 +0200

    check email

--- src/pages/VerifyEmail.tsx
+++ src/pages/VerifyEmail.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { Result, Button } from 'antd'
+import { CheckCircleOutlined, MailOutlined } from '@ant-design/icons'
 
 const VerifyEmail: React.FC = () => {
   return (
-    <div>
-      <h1>Подтвердите свой email</h1>
-    </div>
+    <Result
+      icon={<MailOutlined />}
+      title='Подтвердите свой email'
+      subTitle='Мы отправили вам письмо с инструкциями по подтверждению email. Пожалуйста, проверьте свою почту.'
+      extra={[
+        <Button type='primary' key='back' onClick={() => window.history.back()}>
+          Вернуться назад
+        </Button>,
+        <Button type='primary' key='home' href='/'>
+          На главную
+        </Button>,
+      ]}
+    />
   )
 }
 
