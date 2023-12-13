1099ada86b569b170f9a33d263a22759e245de96
Renat Chernov <renat3846@gmail.com>
2023-12-13 10:05:39 +0200

    commit

--- src/lang/useTranslations.ts
+++
@@ -1,9 +0,0 @@
-import { useTranslation } from 'react-i18next'
-
-const useTranslations = (namespace: string) => {
-  const { t } = useTranslation()
-
-  return (key: string) => t(`${namespace}.${key}`)
-}
-
-export default useTranslations
